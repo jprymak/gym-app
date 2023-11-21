4a1da7a13ce596ab1b8d431064520f30b10c8813
Jakub Prymak <jprymak@gmail.com>
2023-11-21 17:54:21 +0100

    add page header with page name & add light border to each box

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,7 +3,11 @@ import { usePathname } from "next/navigation";
 
 function Header() {
   const pathname = usePathname();
-  return <div className="">{pathname.split("/").pop()}</div>;
+  return (
+    <div className="bg-bg-secondary border border-bg-tertiary px-4 py-6 rounded-md text-xl capitalize">
+      {pathname.split("/").pop()}
+    </div>
+  );
 }
 
 export default Header;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import Sidebar from "../components/sidebar/sidebar";
+import Header from "../components/header/header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
-      <div className="bg-bg-secondary  p-5 min-h-screen">
+      <div className="bg-bg-secondary border border-bg-tertiary p-4 min-h-screen">
         <Sidebar />
       </div>
-      <div className="p-5">{children}</div>
+      <div className="flex-1 p-4">
+        <Header />
+        <div className="p-5">{children}</div>
+      </div>
     </div>
   );
 };
