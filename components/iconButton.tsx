ce4d0cf9aa4288a48ddb78f68170b14d33935b84
Jakub Prymak <jprymak@gmail.com>
2024-01-15 22:14:05 +0100

    feat: disable tooltips on mobile devices

--- components/iconButton.tsx
+++ components/iconButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useWindowSize } from "@/lib/hooks/useWindowSize";
+
 import { Button, ButtonProps } from "./ui/button";
 import {
   Tooltip,
@@ -17,26 +19,35 @@ interface IconButtonProps extends ButtonProps {
 const IconButton = React.forwardRef<
   HTMLButtonElement,
   React.HTMLAttributes<HTMLButtonElement> & IconButtonProps
->(({ tooltip, icon, label = "", ...props }, ref) => (
-  <TooltipProvider delayDuration={300}>
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <span tabIndex={0}>
-          <Button
-            aria-label={tooltip}
-            className="flex gap-2"
-            {...props}
-            ref={ref}
-          >
-            {icon}
-            {label}
-          </Button>
-        </span>
-      </TooltipTrigger>
-      <TooltipContent>{tooltip}</TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
-));
+>(({ tooltip, icon, label = "", ...props }, ref) => {
+  const { screenWidth } = useWindowSize();
+  const isMobileDevice = screenWidth < 1024;
+  return isMobileDevice ? (
+    <Button aria-label={tooltip} className="flex gap-2" {...props} ref={ref}>
+      {icon}
+      {label}
+    </Button>
+  ) : (
+    <TooltipProvider delayDuration={300}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span tabIndex={0}>
+            <Button
+              aria-label={tooltip}
+              className="flex gap-2"
+              {...props}
+              ref={ref}
+            >
+              {icon}
+              {label}
+            </Button>
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>{tooltip}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+});
 IconButton.displayName = "IconButton";
 
 export { IconButton };
