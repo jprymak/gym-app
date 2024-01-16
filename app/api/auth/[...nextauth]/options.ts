9a94400ab8ad95aa2c5d52c58d5aa96e6875ca6f
Jakub Prymak <jprymak@gmail.com>
2024-01-16 14:22:22 +0100

    auth: add basic next-auth configuration

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,42 @@
+// @ts-nocheck
+
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        return {
+          ...profile,
+          role: "admin",
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_Secret,
+    }),
+    GoogleProvider({
+      profile(profile) {
+        return {
+          ...profile,
+          id: profile.sub,
+          role: "admin",
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_Secret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.role = token.role;
+        return session;
+      }
+    },
+  },
+};
