3fb4fc2b67265627eb51c9e2a6b5825f41dd9025
Jakub Prymak <jprymak@gmail.com>
2024-01-22 23:48:32 +0100

    feat: create new users with initial data

--- lib/constants/exercise.ts
+++ lib/constants/exercise.ts
@@ -1,1 +1,24 @@
 export const linkOptions = ["https://youtu.be", "https://www.youtube.com"];
+
+export const initialExercises = [
+  {
+    name: "Bench Press",
+    muscleGroups: ["chest", "triceps"],
+    demoLink: "https://www.youtube.com/watch?v=4Y2ZdHCOXok&t=8s",
+  },
+  {
+    name: "Squat",
+    muscleGroups: ["quadriceps", "hamstrings", "glutes"],
+    demoLink: "https://www.youtube.com/watch?v=gcNh17Ckjgg",
+  },
+  {
+    name: "Conventional Deadlift",
+    muscleGroups: ["hamstrings", "glutes", "back"],
+    demoLink: "https://www.youtube.com/watch?v=VL5Ab0T07e4",
+  },
+  {
+    name: "Bicep curls",
+    muscleGroups: ["biceps"],
+    demoLink: "https://www.youtube.com/watch?v=i1YgFZB6alI",
+  },
+];
